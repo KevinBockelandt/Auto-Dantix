85954219c48971f1f6ba17947989fe4dfdc8baa3
Kevin Bockelandt <Kevin.Bockelandt-CIC@ibm.com>
2024-02-27 11:18:16 +0100

    Better method that monitors changes in order to go faster

--- auto-dantix.js
+++ auto-dantix.js
@@ -1,14 +1,97 @@
 const guesses = [
+  'c',
+  'd',
+  'l',
+  'm',
+  'n',
+  's',
+  'à',
+  'sa',
+  'de',
+  'là',
   'le',
-  'du',
   'un',
+  'il',
+  'ou',
+  'où',
   'ce',
   'ne',
   'en',
-  'il',
-  'si',
-  'de',
-  'ni',
+  'du',
+  'ci',
+  'et',
+  'son',
+  'que',
+  'via',
+  'être',
+  'avoir',
+  'faire',
+  'lire',
+  'devenir',
+  'sud',
+  'est',
+  'ouest',
+  'pays',
+  'ville',
+  'Europe',
+  'Amérique',
+  'Asie',
+  'Afrique',
+  'Australie',
+  'continent',
+  'endroit',
+  'lieu',
+  'France',
+  'Français',
+  'Anglais',
+  'mathématique',
+  'physique',
+  'astronomie',
+  'science',
+  'calcul',
+  'étude',
+  'texte',
+  'mot',
+  'oeuvre',
+  'concept',
+  'idée',
+  'art',
+  'architecture',
+  'bâtiment',
+  'religion',
+  'dieu',
+  'période',
+  'histoire',
+  'X',
+  'XI',
+  'XII',
+  'XIII',
+  'XIV',
+  'XV',
+  'XVI',
+  'XVII',
+  'XVIII',
+  'XIX',
+  'XX',
+  'XXI',
+  'siècle',
+  'après',
+  'avant',
+  'petit',
+  'grand',
+  'moins',
+  'plus',
+  'bien',
+  'mal',
+  'début',
+  'fin',
+  'ancien',
+  'nouveau',
+  'ainsi',
+  'alors',
+  'pour',
+  'encore',
+  'ensemble',
 ];
 
 // useful to use with 'await' keyword
@@ -17,17 +100,30 @@ function sleep(duration) {
 }
 
 async function tryAllGuesses() {
-  // not sure why this is needed. In theory we already wait for the page to be fully loaded
-  await sleep(1000);
+  // wait a little at the start since the pedantix scripts take some time to setup
+  await sleep(500);
 
-  let curIndex = 0;
   const inputField = document.getElementById('pedantix-guess');
   const inputButton = document.getElementById('pedantix-guess-btn');
+  let curPlaceholder = '';
+  let curIndex = 0;
+  let shouldContinue;
 
   while (curIndex < guesses.length) {
     inputField.value = guesses[curIndex];
-    inputButton.click();
-    await sleep(1000);
+    curPlaceholder = inputField.placeholder;
+    shouldContinue = true;
+
+    while (shouldContinue) {
+      if (inputField.placeholder !== curPlaceholder) {
+        shouldContinue = false;
+      } else{
+        await sleep(50);
+        inputField.value = guesses[curIndex];
+        inputButton.click();
+      }
+    }
+
     curIndex += 1;
   }
 }
